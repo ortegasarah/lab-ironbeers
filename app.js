44aabbb7bff0bff918eb177eab4145c8f34ce2da
Sarah Ortega <sarah.ortega.fuentes@gmail.com>
2021-12-02 15:06:06 -0600

    done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
 // ...
 
 // Add the route handlers here:
@@ -24,30 +24,50 @@ app.get('/', (req, res) => {
 
 app.get('/beers', (req, res, next) => {
   punkAPI
-    .getBeer(id)
+    .getBeers()
     .then(beersFromApi => {
       console.log(beersFromApi);
-      res.render('random-beers', {
+      res.render('beers', {
         beersFromApi
       });
     })
     .catch(error => console.log(error));
-
-  res.render('beers');
 });
 
 app.get('/random-beer', (req, res, next) => {
+  let newBeer = {} //objecto
+
   punkAPI
     .getRandom()
     .then(beersFromApi => {
       console.log(beersFromApi)
-      res.render('random-beers', {
-        beersFromApi
+      newBeer = {
+        name: beersFromApi[0].name,
+        tagline: beersFromApi[0].tagline,
+        description: beersFromApi[0].description,
+        image_url: beersFromApi[0].image_url,
+        foodPairing: beersFromApi[0].food_pairing,
+        brewersTips: beersFromApi[0].brewers_tips
+      }
+      res.render('random-beer', {
+        newBeer: [newBeer]
       });
     })
     .catch(error => console.log(error));
-
-  res.render('random-beer');
 });
+
+app.get('/beers/:beerId', (req, res, next) => {
+  punkAPI
+  .getBeer(req.params.beerId)
+  .then(oneBeer =>{
+    console.log(oneBeer)
+    res.render('beer-details', {
+      oneBeer
+    });
+  })
+  .catch(error => console.log(error));
+})
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
